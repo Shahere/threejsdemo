3ddb3c70cc40b4bd32c6e444fb1175bfec77d0c9
Savinien Barbotaud <savinienbarbotaud@gmail.com>
2025-10-19 14:44:38 +0200

    mvp fini

---
+++ router.js
@@ -0,0 +1,36 @@
+let voxels = document.getElementById("voxels");
+let earth = document.getElementById("earth");
+let fps = document.getElementById("fps");
+let fp = document.getElementById("fp");
+let physics = document.getElementById("physics");
+let lightbox = document.getElementById("lightbox");
+
+let main = document.getElementById("homeView");
+
+document.addEventListener("DOMContentLoaded", () => {
+  function fadeAndGo(url) {
+    main.classList.add("hidden");
+    setTimeout(() => {
+      window.location.href = url;
+    }, 500);
+  }
+
+  voxels.addEventListener("click", () => {
+    fadeAndGo("./voxels/index.html");
+  });
+  earth.addEventListener("click", () => {
+    fadeAndGo("./earthspin/index.html");
+  });
+  fps.addEventListener("click", () => {
+    fadeAndGo("./firstpersonshooter/index.html");
+  });
+  fp.addEventListener("click", () => {
+    fadeAndGo("./firstperson/index.html");
+  });
+  physics.addEventListener("click", () => {
+    fadeAndGo("./physics/index.html");
+  });
+  lightbox.addEventListener("click", () => {
+    fadeAndGo("./lightBox/index.html");
+  });
+});
