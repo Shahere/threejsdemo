c7e31f05f15b2e38b078484346e5eb1df8e2573c
Savinien Barbotaud <savinienbarbotaud@gmail.com>
2025-11-27 10:44:24 +0100

    improvement

--- projects/spacecube/src/scene.js
+++ projects/spacecube/src/scene.js
@@ -1,10 +1,9 @@
 import * as THREE from "three";
 console.log(THREE.REVISION);
-import { FontLoader } from "three/addons/loaders/FontLoader.js";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
 
-const loader = new FontLoader();
 const scene = new THREE.Scene();
+scene.fog = new THREE.FogExp2(0x000000, 0.02);
 //scene.background = new THREE.Color(0xcccccc);
 
 const camera = new THREE.PerspectiveCamera(
@@ -21,17 +20,17 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 const controls = new OrbitControls(camera, renderer.domElement);
 
-const principalLight = new THREE.PointLight(0xffffff, 15);
+const principalLight = new THREE.PointLight(0x00ff00, 50);
 const principalLightHelper = new THREE.PointLightHelper(principalLight);
 principalLight.position.set(0, 0, 0);
 scene.add(principalLight);
-//scene.add(principalLightHelper);
+scene.add(principalLightHelper);
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.03);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
 scene.add(ambientLight);
 
 const material = new THREE.MeshStandardMaterial();
-material.color = new THREE.Color(0x00ff00);
+material.color = new THREE.Color(0xffffff);
 const geometry = new THREE.BoxGeometry();
 
 function getCube() {
@@ -39,15 +38,38 @@ function getCube() {
   return mesh;
 }
 
-const numBoxes = 50;
-const radius = 15;
-for (let i = 0; i < numBoxes; i++) {
-  const box = getCube();
-  box.position.x = Math.random() * radius - radius * 0.5;
-  box.position.y = Math.random() * radius - radius * 0.5;
-  box.position.z = Math.random() * radius - radius * 0.5;
-  scene.add(box);
+function getRandomSpherePoint({ radius = 10 }) {
+  const minRadius = 3;
+  const maxRadius = radius - minRadius;
+  const range = Math.random() * maxRadius + minRadius;
+  const u = Math.random();
+  const v = Math.random();
+  const theta = 2 * Math.PI * u;
+  const phi = Math.acos(2 * v - 1);
+  return {
+    x: range * Math.sin(phi) * Math.cos(theta),
+    y: range * Math.sin(phi) * Math.sin(theta),
+    z: range * Math.cos(phi),
+  };
 }
+
+function getLayout(numBoxes = 500, radius = 100) {
+  for (let i = 0; i < numBoxes; i++) {
+    const box = getCube();
+    //Reste bien pour de la position random sous forme de cube
+    /*
+    box.position.x = Math.random() * radius - radius * 0.5;
+    box.position.y = Math.random() * radius - radius * 0.5;
+    box.position.z = Math.random() * radius - radius * 0.5;
+    */
+
+    //Position en forme de sphere
+    const { x, y, z } = getRandomSpherePoint({ radius });
+    box.position.set(x, y, z);
+    scene.add(box);
+  }
+}
+getLayout();
 
 window.addEventListener("resize", onWindowResize, false);
 function onWindowResize() {
